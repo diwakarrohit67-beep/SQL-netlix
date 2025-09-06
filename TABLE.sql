33ed04818622118cea689b08e4510ac7a0b483ec
diwakarrohit67-beep <diwakarrohit67@gmail.com>
2025-09-06 14:35:30 +0530

    Add files via upload

---
+++ TABLE.sql
@@ -0,0 +1,25 @@
+CREATE TABLE NETFLIX(
+
+show_id VARCHAR(10),
+type VARCHAR(10),
+title VARCHAR(150),	
+director VARCHAR(300),
+casts	VARCHAR(1000),
+country VARCHAR(150),
+date_added VARCHAR(50),
+release_year VARCHAR(50),	
+rating	 VARCHAR(50),
+duration VARCHAR(15),
+listed_in	VARCHAR(100),
+description VARCHAR(250)
+
+);
+
+
+
+
+
+
+
+
+
